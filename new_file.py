786e1cda60e05a45369a6f149fb5a8aeaf5b75a2
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-01-23 22:01:34 +0100

    Update new_file.py

--- new_file.py
+++ new_file.py
@@ -3,13 +3,15 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-html_text=requests.get("https://sofifa.com/").text
+html_text=requests.get("https://sofifa.com/players?r=220069&set=true").text
 soup = BeautifulSoup(html_text, 'lxml')
 player_list=[]
-players=soup.find_all('tr')
+alll=soup.find('table',class_='table table-hover persist-area')
+print(soup.prettify())
+players=soup.find('tr')
 for player in players:
     player_name=player.find('div',class_='ellipsis')
-    #print(player_name)
+    print(player_name)
     player_age=player.find('td',class_='col-ae')
     player_team=player.find("a")
     player_value=player.find('td',class_='col-vl')
