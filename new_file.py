dc9136e171831d66f28fe9c95b9fbc30a8c69660
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-01-24 16:18:34 +0100

    Update new_file.py

--- new_file.py
+++ new_file.py
@@ -1,21 +1,57 @@
+from pickle import NONE
 import time
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-html_text=requests.get("https://sofifa.com/players?r=220069&set=true").text
-soup = BeautifulSoup(html_text, 'lxml')
-player_list=[]
-alll=soup.find('table',class_='table table-hover persist-area')
-print(soup.prettify())
-players=soup.find('tr')
-for player in players:
-    player_name=player.find('div',class_='ellipsis')
-    print(player_name)
-    player_age=player.find('td',class_='col-ae')
-    player_team=player.find("a")
-    player_value=player.find('td',class_='col-vl')
-    player_contract=player.find('div',class_='sub')
-    player_list.append([player_name,player_age,player_team,player_value,player_contract])
+def extract_source(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+    source=requests.get(url, headers=headers).text
+    return source
 
-#print(player_stats())
+html_text=extract_source("https://sofifa.com/players?r=220069&set=true")
+soup = BeautifulSoup(html_text, 'html.parser')
+player_list=[]
+print(soup.prettify())
+
+#alll=soup.find('table',class_='table table-hover persist-area')
+#print(soup.prettify())
+player_data={}
+players=soup.find_all('tr')
+print(type(players))
+def controle(x):
+    if x is not None:
+        x=x.text
+    return x
+
+www=players[0]
+
+for player in players:  
+    if player!=www:
+        player_name=player.find('div',class_='ellipsis')
+        player_name=controle(player_name)
+
+        player_age=player.find('td',class_='col-ae')
+        player_age=controle(player_age)
+
+
+        player_team=player.find('figure',class_="avatar avatar-sm transparent").next_sibling.next_sibling
+        player_team=controle(player_team)
+        print(player_team)
+
+        player_value=player.find('td',class_='col-vl')
+        player_value=controle(player_value)
+
+        player_contract=player.find('div',class_='sub')
+        player_contract=controle(player_contract)
+
+        player_data = {
+            "name" : player_name,
+            "age" :player_age,
+            "team":player_team,
+            "value":player_value,
+            "contract":player_contract
+        }
+        player_list.append(player_data)
+
+print(player_list)
