78519301b964b0ce1124e313aa97a328416686fe
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-01-25 10:46:13 +0100

    some changes

--- new_file.py
+++ new_file.py
@@ -9,55 +9,76 @@ def extract_source(url):
     source=requests.get(url, headers=headers).text
     return source
 
-html_text=extract_source("https://sofifa.com/players?r=220069&set=true")
-soup = BeautifulSoup(html_text, 'html.parser')
-player_list=[]
-player_data={}
-players=soup.find_all('tr')
 def controle(x):
     if x is not None:
         x=x.text
     return x
 
-www=players[0]
 
-for player in players:  
-    if player!=www:
-        player_name=player.find('div',class_='ellipsis')
-        player_name=controle(player_name)
-
-        player_age=player.find('td',class_='col-ae')
-        player_age=controle(player_age)
+    
+df=pd.DataFrame()
 
 
-        player_team=player.find('figure',class_="avatar avatar-sm transparent").next_sibling.next_sibling
-        player_team=controle(player_team)
+for i in range(1,5):
+    html_text=extract_source(f"https://sofifa.com/players?r=220069&offset={i*60}")
+    soup = BeautifulSoup(html_text, 'html.parser')
+    player_list=[]
+    player_data={}
+    players=soup.find_all('tr')
 
-        player_value=player.find('td',class_='col-vl')
-        player_value=controle(player_value)
+    www=players[0]
 
-        player_contract=player.find('div',class_='sub')
-        player_contract=controle(player_contract)
-        player_contract=player_contract[1:]
+    for player in players:  
+        if player!=www:
+            player_name=player.find('div',class_='ellipsis')
+            player_name=controle(player_name)
 
-        player_post=[]
-        player_posts=player.find_all('span',class_='pos')
-        for i in player_posts:
-            i=i.text
-            player_post.append(i)
+            player_age=player.find('td',class_='col-ae')
+            player_age=controle(player_age)
 
-        player_data = {
-            "name" : player_name,
-            "age" :player_age,
-            "team":player_team,
-            "value":player_value,
-            "contract":player_contract,
-            "Posts":player_post,
-        }
-        player_list.append(player_data)
 
-df = pd.DataFrame(player_list)
-print(df.head())
-df.to_csv('datadata.csv',index=False)
+            player_team=player.find('figure',class_="avatar avatar-sm transparent")
+            if player_team is not None:
+                player_team=player_team.next_sibling.next_sibling
+            player_team=controle(player_team)
+
+            player_rating=player.find('td',class_='col-oa')
+            if player_rating is not None:
+                player_rating=player_rating.span
+            player_rating=controle(player_rating)
+
+            player_value=player.find('td',class_='col-vl')
+            player_value=controle(player_value)
+
+            player_wage=player.find('td',class_='col-wg')
+            player_wage=controle(player_wage)
+
+            player_contract=player.find('div',class_='sub')
+            player_contract=controle(player_contract)
+            player_contract=player_contract[1:]
+
+
+            player_post=[]
+            player_posts=player.find_all('span',class_='pos')
+            for i in player_posts:
+                i=i.text
+                player_post.append(i)
+
+            player_data = {
+                "name" : player_name,
+                "age" :player_age,
+                "team":player_team,
+                "rating":player_rating,
+                "value":player_value,
+                "wage":player_wage,
+                "contract":player_contract,
+                "Posts":player_post,
+            }
+            player_list.append(player_data)
+    
+
+    df=pd.concat([df,pd.DataFrame(player_list)])
+    df.to_csv('ayoubs.csv',index=False)
+
 
 #print(player_list)
