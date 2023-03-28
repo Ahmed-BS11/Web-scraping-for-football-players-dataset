6290824cebaeb735e823b4316c70edfa705b14ba
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-03-28 12:01:57 +0100

    2019

--- 2022_2023season.py
+++ 2022_2023season.py
@@ -3,67 +3,67 @@ from bs4 import BeautifulSoup
 from csv import writer
 
 
-
-with open('DebutFin2021_2022.csv', 'w', newline='', encoding='utf8') as file:
+with open('DebutFin2019_2020.csv', 'a', newline='', encoding='utf8') as file:
         thewriter = writer(file)
-        header = ['name', 'age', 'position', 'Country', 'MarketValue', 'PreviousTeam', 'LeagueOfPreviousTeam','CountryOfPreviousTeam', 'Fee', 'YearOfTranfert', 'NewTeam', 'LeagueOfNewTeam', 'CountryOfNewTeam',
-                  'Height', 'Agent', 'JoinedCurrentTeam', 'ContratExpires','Squad', 'Appearances', 'PPG', 'Goals', 'Assists', 'OwnGoals', 'SubsON' ,'SubsOFF' ,'YellowCards' ,'SecondYellowCards' ,'RedCards' ,'PenaltyGoals' ,'MinutesPerGoal' ,'MinutesPlayed', 'PlaceOfBirth', 'DateOfBirth'] #les noms des colonnes du fichier
-        thewriter.writerow(header) #first row contains the header values
+        header = ['name', 'age', 'position', 'Country', 'MarketValue', 'PreviousTeam', 'LeagueOfPreviousTeam', 'CountryOfPreviousTeam', 'Fee', 'YearOfTranfert', 'NewTeam', 'LeagueOfNewTeam', 'CountryOfNewTeam',
+                  'Height', 'Agent', 'JoinedCurrentTeam', 'ContratExpires', 'Squad', 'Appearances', 'PPG', 'Goals', 'Assists', 'OwnGoals', 'SubsON', 'SubsOFF', 'YellowCards', 'SecondYellowCards', 'RedCards', 'PenaltyGoals', 'MinutesPerGoal', 'MinutesPlayed', 'PlaceOfBirth', 'DateOfBirth']  # les noms des colonnes du fichier
+        thewriter.writerow(header)  # first row contains the header values
 
-        for num_page in range(1, 41):
-                URL = f"https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/plus/1/galerie/0?saison_id=2021&page="+ str(num_page)
-                HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}  # solution to 403 forbidden
+        for num_page in range(1, 81):
+                URL = f"https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/plus/1/galerie/0?saison_id=2019&page=" + \
+                    str(num_page)
+                # solution to 403 forbidden
+                HEADERS = {
+                    'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
 
                 response = requests.get(URL, headers=HEADERS)
                 print(response)
 
-                soup = BeautifulSoup(response.content, 'html.parser')  #the whole page content
+                # the whole page content
+                soup = BeautifulSoup(response.content, 'html.parser')
                 # print(soup.prettify()) #Ou bien tt simplement print(response.text)
 
-
-
-
-                #Debut jiben URLS
+                # Debut jiben URLS
                 keysS = soup.find('div', class_='keys').find_all('span')
                 # print(keysS[7].text)
                 keys = [keys.text for keys in
                         keysS]  # liste contient les numeros des joueurs qui apparaissent dans la page.
-                #print(keys)
+                # print(keys)
 
                 rows = soup.find('table', class_='items').find('tbody')
                 namesS = [row.find('a') for row in rows if row.find('a') != -1]
                 names = [names.text for names in
                          namesS]  # liste contient les noms des joueurs qui apparaissent dans la page.
-                #print(names)
+                # print(names)
                 D = {" ": "", "ä": "a", "ç": "c", "è": "e", "º": "", "Ã": "A", "Í": "I", "í": "i", "Ü": "U", "â": "a",
                      "ò": "o", "¿": "", "ó": "o", "á": "a", "à": "a", "õ": "o", "¡": "", "Ó": "O", "ù": "u", "Ú": "U",
                      "´": "", "Ñ": "N", "Ò": "O", "ï": "i", "Ï": "I", "Ç": "C", "À": "A", "É": "E", "ë": "e", "Á": "A",
                      "ã": "a", "Ö": "O", "ú": "u", "ñ": "n", "é": "e", "ê": "e", "·": "-", "ª": "a", "°": "", "ü": "u",
-                     "ô": "o","ć":"c", "'":""}
+                     "ô": "o", "ć": "c", "'": ""}
                 listURL = []
                 for name, key in zip(range(len(names)), range(len(keys))):
-                    url = "https://www.transfermarkt.com/" + str(names[name]).replace('.','-') + "/leistungsdatendetails/spieler/" + str(
-                        keys[key]) + "/saison/2021/verein/0/liga/0/wettbewerb//pos/0/trainer_id/0/plus/1"
+                    url = "https://www.transfermarkt.com/" + str(names[name]).replace('.', '-') + "/leistungsdatendetails/spieler/" + str(
+                        keys[key]) + "/saison/2019/verein/0/liga/0/wettbewerb//pos/0/trainer_id/0/plus/1"
                     for ghalet, shyh in D.items():
                         url = url.replace(ghalet, shyh)
                     listURL.append(url)
-                #print(listURL)
-                #Fin jiben URLS
-
+                # print(listURL)
+                # Fin jiben URLS
 
                 table = soup.find('table', class_='items')
                 if table is not None:
                         rows = table.find('tbody')
                 # print(rows)
 
-                #names = [row.find('a') for row in rows if row.find('a')!= -1]
-                td = [row.find_all('td') for row in rows if row != '\n']#find all td
-                 #print(td[0])
+                # names = [row.find('a') for row in rows if row.find('a')!= -1]
+                td = [row.find_all('td')
+                                   for row in rows if row != '\n']  # find all td
+                 # print(td[0])
                 a = [row.find_all('a') for row in rows if row != '\n']
-                #print(a[0])
+                # print(a[0])
                 img = [row.find_all('img') for row in rows if row != '\n']
-                 #print(img[1][-3])
-                 #names1 = [row.find('img').get('title') for row in rows if row.find('a')!= -1]
+                 # print(img[1][-3])
+                 # names1 = [row.find('img').get('title') for row in rows if row.find('a')!= -1]
 
                 for i, j, k in zip(range(len(td)), range(len(a)), range(len(img))):
                         name = td[i][3].text.replace('\n', "")
@@ -72,34 +72,52 @@ with open('DebutFin2021_2022.csv', 'w', newline='', encoding='utf8') as file:
                         MarketValue = td[i][6].text.replace('\n', "")
                         Fee = td[i][-1].text.replace('\n', "")
                         PreviousTeam = a[j][1].get('title').replace('\n', "")
-                        LeagueOfPreviousTeam = a[j][3].get('title').replace('\n', "")
+                        LeagueOfPreviousTeam = a[j][3].get(
+                            'title').replace('\n', "")
                         NewTeam = a[j][4].get('title').replace('\n', "")
                         LeagueOfNewTeam = a[j][-1].text.replace('\n', "")
                         Country = img[k][1].get('alt').replace('\n', "")
-                        CountryOfPreviousTeam = img[k][-3].get('alt').replace('\n', "")
-                        CountryOfNewTeam = img[k][-1].get('alt').replace('\n', "")
-                        YearOfTransfert = "2021"
+                        CountryOfPreviousTeam = img[k][-3].get(
+                            'alt').replace('\n', "")
+                        CountryOfNewTeam = img[k][-1].get(
+                            'alt').replace('\n', "")
+                        YearOfTransfert = "2019"
                         autreURL = listURL[i]
                         print(autreURL)
                         response = requests.get(autreURL, headers=HEADERS)
-                        #print(response)
+                        # print(response)
                         soupp = BeautifulSoup(response.content, 'html.parser')
 
-                        dataheadercontent = soupp.find_all('span', class_='data-header__content')
+                        dataheadercontent = soupp.find_all(
+                            'span', class_='data-header__content')
                         height = soupp.find('span', itemprop='height')
-                        if height != None :
-                                Height = height.text.replace('\n',"")
+                        if height != None:
+                                Height = height.text.replace('\n', "")
                         else:
                                 Height = "NONE"
 
-                        agent = soupp.find('span', class_='data-header__content data-header__content--vertical-aligned')
-                        if agent != None :
-                                Agent = agent.text.replace('\n',"")
+                        agent = soupp.find(
+                            'span', class_='data-header__content data-header__content--vertical-aligned')
+                        if agent != None:
+                                Agent = agent.text.replace('\n', "")
                         else:
                                 Agent = "NONE"
 
-                        JoinedCurrentTeam = dataheadercontent[1].text.replace('\n',"")
-                        ContratExpires = dataheadercontent[2].text.replace('\n',"")
+                        #JoinedCurrentTeam = dataheadercontent[1].text.replace('\n', "")
+                        try:
+                            JoinedCurrentTeam = dataheadercontent[1].text.replace('\n',"")
+                            ContratExpires = dataheadercontent[2].text.replace('\n',"")
+                            PlaceOfBirth = dataheadercontent[4].text.replace('\n',"")
+                            DateOfBirth = dataheadercontent[3].text.replace('\n',"")
+                        except IndexError:
+                            # Handle the situation when the index is out of range
+                            #print("Error: The dataheadercontent list does not contain at least two elements")
+                            JoinedCurrentTeam = ""
+                            ContratExpires = ""
+                            PlaceOfBirth= ""
+                            DateOfBirth= ""
+
+                        
                         TD = soupp.find_all('td', class_="zentriert")
 
                         if TD != []:
@@ -140,9 +158,6 @@ with open('DebutFin2021_2022.csv', 'w', newline='', encoding='utf8') as file:
                         else:
                                 MinutesPerGoal = "NONE"
                                 MinutesPlayed = "NONE"
-
-                        PlaceOfBirth = dataheadercontent[4].text.replace('\n',"")
-                        DateOfBirth = dataheadercontent[3].text.replace('\n',"")
 
                         dataset = [name, age, position, Country, MarketValue,PreviousTeam, LeagueOfPreviousTeam,CountryOfPreviousTeam, Fee, YearOfTransfert, NewTeam, LeagueOfNewTeam, CountryOfNewTeam,
                                    Height,Agent,JoinedCurrentTeam,ContratExpires,Squad,Appearances,PPG,Goals,Assists,OwnGoals,SubsON,SubsOFF,YellowCards,SecondYellowCards,RedCards,PenaltyGoals,MinutesPerGoal,MinutesPlayed,PlaceOfBirth,DateOfBirth]
