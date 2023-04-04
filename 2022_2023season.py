69898e86b5ab084a393bf113dfd4de822d6cccb6
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-04-04 15:28:06 +0100

    2005

--- 2022_2023season.py
+++ 2022_2023season.py
@@ -7,9 +7,9 @@ with open('DebutFin2005_2006.csv', 'a', newline='', encoding='utf8') as file:
         thewriter = writer(file)
         header = ['name', 'age', 'position', 'Country', 'MarketValue', 'PreviousTeam', 'LeagueOfPreviousTeam', 'CountryOfPreviousTeam', 'Fee', 'YearOfTranfert', 'NewTeam', 'LeagueOfNewTeam', 'CountryOfNewTeam',
                   'Height', 'Agent', 'JoinedCurrentTeam', 'ContratExpires', 'Squad', 'Appearances', 'PPG', 'Goals', 'Assists', 'OwnGoals', 'SubsON', 'SubsOFF', 'YellowCards', 'SecondYellowCards', 'RedCards', 'PenaltyGoals', 'MinutesPerGoal', 'MinutesPlayed', 'PlaceOfBirth', 'DateOfBirth']  # les noms des colonnes du fichier
-        thewriter.writerow(header)  # first row contains the header values
+        #thewriter.writerow(header)  # first row contains the header values
 
-        for num_page in range(39, 81):
+        for num_page in range(1,39):
                 URL = f"https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/plus/1/galerie/0?saison_id=2005&page=" + \
                     str(num_page)
                 # solution to 403 forbidden
