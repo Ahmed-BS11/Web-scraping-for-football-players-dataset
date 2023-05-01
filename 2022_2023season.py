cd862b5f807a66a31815039fde4c711a8db1e36a
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-05-01 14:30:21 +0100

    2006

--- 2022_2023season.py
+++ 2022_2023season.py
@@ -9,7 +9,7 @@ with open('2005_2006.csv', 'a', newline='', encoding='utf8') as file:
                   'Height', 'Agent', 'JoinedCurrentTeam', 'ContratExpires', 'Squad', 'Appearances', 'PPG', 'Goals', 'Assists', 'OwnGoals', 'SubsON', 'SubsOFF', 'YellowCards', 'SecondYellowCards', 'RedCards', 'PenaltyGoals', 'MinutesPerGoal', 'MinutesPlayed', 'PlaceOfBirth', 'DateOfBirth']  # les noms des colonnes du fichier
         #thewriter.writerow(header)  # first row contains the header values
 
-        for num_page in range(18,81):
+        for num_page in range(77,81):
                 URL = f"https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/plus/1/galerie/0?saison_id=2005&page=" + \
                     str(num_page)
                 # solution to 403 forbidden
